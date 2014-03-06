8ca5db80d0c16703e2b26554b1f0d1b70f1857d6
Cody Wilson & Kelly Becker <student@epicodus.com>
2014-03-06 14:18:36 -0800

    working with exp. hack - missing divide by zero

--- lib/word_calculator.rb
+++ lib/word_calculator.rb
@@ -1,22 +1,27 @@
 def calculator(originalString)
 
-  operators = {"plus" => :+, "minus" => :-, "divided" => :/, "times" => :*, "power"=> :**} 
-  originalString=originalString.sub!(/[^a-zA-Z0-9_\s.]/, "")
-  words=originalString.split
-  answer=0
+  operators = {"plus" => :+, "minus" => :-, "divided" => :/, "times" => :*, "power" => :**} 
+  #originalString=originalString.sub!(/[^a-zA-Z0-9_\s.]/, "")
+  questions=originalString.split("?")
+  answer=[]
   calculations = []
+  words = []
 
-  puts "#{calculations}"
-
-  words.each do |word|
-    if operators.include? word
-        calculations.push(operators[word])
-    elsif /[0-9]/.match(word) 
-        calculations.push(word.to_f)
-    else 
-        calculations = calculations
-    end
-  end
-
-  calculations = eval(calculations.join)
+  questions.each do |question|
+    words=question.split
+      words.each do |word|
+        if operators.include? word
+            calculations.push(operators[word])
+        elsif /[0-9]/.match(word) 
+            calculations.push(word.to_f)
+        else 
+            calculations = calculations
+        end
+      end 
+      #puts "#{calculations}"
+     answer.push(eval(calculations.join)) 
+     calculations=[]
+  end   
+  #puts "#{answer}"
+  answer.join(",")
 end
