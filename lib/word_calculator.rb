01b234da44d1ac89277312691da68de6ba7471b1
Cody Wilson & Kelly Becker <student@epicodus.com>
2014-03-06 14:47:49 -0800

    completes powers with first, second...hash

--- lib/word_calculator.rb
+++ lib/word_calculator.rb
@@ -1,7 +1,20 @@
 def calculator(originalString)
 
-  operators = {"plus" => :+, "minus" => :-, "divided" => :/, "times" => :*, "power" => :**} 
-  #originalString=originalString.sub!(/[^a-zA-Z0-9_\s.]/, "")
+  operators = {"plus" => :+,
+              "minus" => :-,
+              "divided" => :/,
+              "times" => :*,
+              "first" => [:**,1],
+              "second" => [:**,2],
+              "third" => [:**,3],
+              "fourth" => [:**,4],
+              "fifth" => [:**,5],
+              "sixth" => [:**,6],
+              "seventh" => [:**,7],
+              "eighth" => [:**,8],
+              "nineth" => [:**,9],
+              "tenth" => [:**,10]} 
+
   questions=originalString.split("?")
   answer=[]
   calculations = []
