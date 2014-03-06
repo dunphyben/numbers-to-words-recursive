8137ec8d7a8334a980b58026b9f6eb1e39767780
Cody Wilson & Kelly Becker <student@epicodus.com>
2014-03-06 11:46:57 -0800

    works for powers and decimals

--- lib/word_calculator.rb
+++ lib/word_calculator.rb
@@ -1,16 +1,18 @@
 def calculator(originalString)
 
-  operators = {"plus" => :+, "minus" => :-, "divided" => :/, "times" => :*}
-  originalString=originalString.sub!(/[^a-zA-Z0-9_\s]/, "")
+  operators = {"plus" => :+, "minus" => :-, "divided" => :/, "times" => :*, "power"=> :**} 
+  originalString=originalString.sub!(/[^a-zA-Z0-9_\s.]/, "")
   words=originalString.split
   answer=0
   calculations = []
+
+  puts "#{calculations}"
 
   words.each do |word|
     if operators.include? word
         calculations.push(operators[word])
     elsif /[0-9]/.match(word) 
-        calculations.push(word)
+        calculations.push(word.to_f)
     else 
         calculations = calculations
     end
